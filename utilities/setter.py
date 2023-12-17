a62e4f27bfc885c2ed15ed79f28b4d8f533d057c
FabrizioVasquez <fabrizio.vx@gmail.com>
2023-12-17 00:20:23 -0500

    Adding workflow for testing api and adding utilities

---
+++ utilities/setter.py
@@ -0,0 +1,12 @@
+import os 
+from base64 import b64decode # usamos esto que vamos a usar el secret que estara en service acount key, esta codeado en 64 y debemos decodearlo
+
+def main():
+    key = os.environ.get('SERVICE_ACCOUNT_KEY')
+    with open ('path.json','w') as json_file:
+        json_file.write(b64decode(key.decode()))
+
+    print(os.path.realpath('path.json'))
+
+if __name__ == '__main__':
+    main()
