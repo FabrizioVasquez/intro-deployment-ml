2a854445eb8cce2d74ad0a2c7d4ff48e78e183e1
FabrizioVasquez <fabrizio.vx@gmail.com>
2023-12-17 00:34:27 -0500

    Moving testing files to root dir

--- tests.py
+++ tests.py
@@ -18,3 +18,18 @@ def test_null_prediction():
 })
     assert response.status_code == 200
     assert response.json()['worldwide_gross'] == 0
+
+def test_random_prediction():
+    response = client.post('/v1/prediction',json={
+  "opening_gross": 8330681,
+  "screens": 2271,
+  "production_budget": 13000000,
+  "title_year": 1999,
+  "aspect_ratio": 1.85,
+  "duration": 97,
+  "cast_total_facebook_likes": 37907,
+  "budget": 16000000,
+  "imdb_score": 7.3
+})
+    assert response.status_code == 200
+    assert response.json()['worldwide_gross'] != 0
